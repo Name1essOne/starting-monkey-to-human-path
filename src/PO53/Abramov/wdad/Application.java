237656d20937086a8af65f29c4ad749dc2fa852e
amax1 <a.max1m@mail.ru>
2017-09-19 14:06:25 +0400

    Added entry point

---
+++ src/PO53/Abramov/wdad/Application.java
@@ -0,0 +1,10 @@
+package PO53.Abramov.wdad;
+
+/**
+ * Created by amax1 on 19.09.2017.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.print("I’m  Maxim Abramov, and I’m not a monkey");
+    }
+}
