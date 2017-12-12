d64c195554fcbab0d058c169cc102da3e9de347c
amax1 <a.max1m@mail.ru>
2017-12-12 14:37:40 +0400

    RMI 1

--- src/PO53/Abramov/wdad/Application.java
+++ src/PO53/Abramov/wdad/Application.java
@@ -14,44 +14,6 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("I’m  Maxim Abramov, and I’m not a monkey");
-
-        XmlTask xmlTask = new XmlTask("C:\\Users\\amax1\\starting-monkey-to-human-path\\src\\PO53\\Abramov\\wdad\\learn\\xml\\Test.xml");
-
-        System.out.println(xmlTask.getDoc().getElementsByTagName("officiant").item(0).getAttributes().item(0).getNodeValue());
-
-        System.out.println(xmlTask.getDoc().getElementsByTagName("date").item(0).getAttributes().item(0).getNodeValue());
-
-        System.out.println(xmlTask.getDoc().getChildNodes().getLength());
-        System.out.println(xmlTask.getDoc().getChildNodes().item(1).getChildNodes().item(0).getNodeName());
-
-        System.out.println(xmlTask.getDoc().getElementsByTagName("date").item(0).getChildNodes().item(0).getChildNodes().item(5).getFirstChild().getNodeValue());
-
-        PreferencesManager preferencesManager = new PreferencesManager("C:\\Users\\amax1\\starting-monkey-to-human-path\\src\\PO53\\Abramov\\wdad\\resources\\configuration\\appconfig.xml");
-
-        //System.out.println(preferencesManager.getPolicypath());
-
-        Calendar c = new GregorianCalendar(2005,2,13);
-
-        System.out.println(xmlTask.earningsTotal("Sidorov", c));
-
-        System.out.println(preferencesManager.getPolicypath());
-
-        System.out.println(preferencesManager.getClassprovider());
-
-        System.out.println(preferencesManager.getCreateregistry());
-
-        System.out.println(preferencesManager.getRegistryaddress());
-
-        System.out.println(preferencesManager.getUsecodebaseonly());
-
-        System.out.println(xmlTask.numberOfItemsPerDay(c,"sausage"));
-
-        //System.out.println(xmlTask.getDoc().getElementsByTagName("totalcost").item(0).getChildNodes().);
-
-        //xmlTask.getDoc().getElementsByTagName("officiant").getLength(); кол-во эл-ов
-
-        //System.out.print(xmlTask.earningsTotal( "Petrov")); РАБОТАЕТ!
 
     }
 }
--- src/PO53/Abramov/wdad/data/managers/PreferencesManager.java
+++ src/PO53/Abramov/wdad/data/managers/PreferencesManager.java
@@ -1,27 +1,107 @@
 package PO53.Abramov.wdad.data.managers;
 
 import org.w3c.dom.Document;
-
+import PO53.Abramov.wdad.utils.PreferencesConstantManager;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.Enumeration;
+import java.util.Properties;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 
 /**
  * Created by amax1 on 01.11.2017.
  */
 public class PreferencesManager {
-    private File file;
+    private static volatile PreferencesManager instance;
     private Document doc;
-    private String pathname;
+    private static final String FILE_PATH="C:\\Users\\amax1\\starting-monkey-to-human-path\\src\\PO53\\Abramov\\wdad\\resources\\configuration\\appconfig.xml";
 
-    public PreferencesManager(String pathname) throws Exception {
-        file = new File(pathname);
+    private PreferencesManager() throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setValidating(true);
-        factory.setIgnoringElementContentWhitespace(true);
+        factory.setValidating(false);
         DocumentBuilder builder = factory.newDocumentBuilder();
-        doc = builder.parse(file);
-        this.pathname = pathname;
+        doc = builder.parse(new File(FILE_PATH));
+    }
+
+    public static PreferencesManager getInstance() throws Exception {
+        if (instance == null)
+            synchronized (PreferencesManager.class) {
+                if (instance == null)
+                    instance = new PreferencesManager();
+            }
+
+        return instance;
+    }
+
+    public void setProperty(String key, String value) {
+        getElement(key).setTextContent(value);
+    }
+
+    public String getProperty(String key) {
+        return getElement(key).getTextContent();
+    }
+
+    public void setProperties(Properties prop) {
+        Enumeration elements = prop.elements();
+        Enumeration keys = prop.keys();
+        while (elements.hasMoreElements()) {
+            getElement((String) elements.nextElement()).setTextContent((String) keys.nextElement());
+        }
+    }
+
+    public Properties getProperties() {
+        Properties prop = new Properties();
+        String[] keys = new String[]{
+                PreferencesConstantManager.CREATEREGISTRY,
+                PreferencesConstantManager.REGISTRYADDRESS,
+                PreferencesConstantManager.REGISTRYPORT,
+                PreferencesConstantManager.POLICYPATH,
+                PreferencesConstantManager.USECODEBASEONLY,
+                PreferencesConstantManager.CLASSPROVIDER
+        };
+
+        for (String key : keys) {
+            setProperty(key, getElement(key).getTextContent());
+        }
+
+        return prop;
+    }
+
+    public void addBindedObject(String name, String className) {
+        Element element=(Element) doc.createElement("bindedobject");
+        element.setAttribute("class",className);
+        element.setAttribute("name",name);
+        getElement("rmi").appendChild(element);
+    }
+
+    public void removeBindedObject(String name) {
+        NodeList nodeList = doc.getElementsByTagName("bindedobject");
+        Element element;
+        for (int i = 0; i < nodeList.getLength(); i++  ) {
+            element = (Element) nodeList.item(i);
+            if (element.getAttribute("name").equals(name))
+            {
+                getElement("rmi").removeChild(element);
+            }
+        }
+    }
+
+    private Element getElement(String nameField) {
+        NodeList nodeList = doc.getElementsByTagName(nameField);
+        String[] field = nameField.split("\\.");
+        nodeList = doc.getElementsByTagName(field[field.length - 1]);
+        Node node = nodeList.item(0);
+        return (Element) node;
     }
 
     public Document getDoc() {
@@ -77,4 +157,6 @@ public class PreferencesManager {
         public void setClassprovider(String classprovider) {
             doc.getElementsByTagName("classprovider").item(0).setNodeValue(classprovider);
         }
+
+
 }
